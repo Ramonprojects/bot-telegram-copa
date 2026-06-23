9bf3f7a45a79623edb1e0f505ef6edd1bc566f70
Ramonprojects <adm@ramonmago.com>
2026-06-23 00:56:09 -0300

    chore: shareCode PFY93XYH2WC -> HLOCDRTKV8U (via painel)

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252'
 
-    const shareCode = '&shareCode=PFY93XYH2WC'
+    const shareCode = '&shareCode=HLOCDRTKV8U'
     
     const afp = '&afp5=bot'
     

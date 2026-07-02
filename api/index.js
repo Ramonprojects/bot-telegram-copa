492b1952bc015cabea7346b9769d87d252b3cb10
Ramonprojects <adm@ramonmago.com>
2026-07-01 21:22:24 -0300

    chore: shareCode AOULSA6DMLC -> IKTF4KCCLSJ (via painel)

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252'
 
-    const shareCode = '&shareCode=AOULSA6DMLC'
+    const shareCode = '&shareCode=IKTF4KCCLSJ'
     
     const afp = '&afp5=bot'
     

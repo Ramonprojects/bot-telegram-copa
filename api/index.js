5db6c65e1c5586fa85eaeb8018398affc29d04ce
Ramonprojects <adm@ramonmago.com>
2026-07-08 21:52:31 -0300

    chore: shareCode PNR41ASNBYO -> PPNMK65E92Z (via painel)

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252'
 
-    const shareCode = '&shareCode=PNR41ASNBYO'
+    const shareCode = '&shareCode=PPNMK65E92Z'
     
     const afp = '&afp5=bot'
     
